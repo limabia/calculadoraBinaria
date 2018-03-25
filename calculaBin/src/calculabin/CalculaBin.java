c9f914d2d37c2cd4514052543d4fcc30dcdcf6b1
limabia <bianca.lima.1504@gmail.com>
2018-03-25 17:56:18 -0300

    adc arquivos iniciais para o projeto

---
+++ calculaBin/src/calculabin/CalculaBin.java
@@ -0,0 +1,18 @@
+
+package calculabin;
+
+public class CalculaBin {
+    // func converte int em bin 
+    // func soma 
+    // func subtrai
+    // func divide
+    // func multiplica
+    // func recebe numero float e manda matissa para as demais
+    
+    public static void main(String[] args) {
+        // recebe o numero do usuario
+        // chama func e dps responde o usuario
+        System.out.println("oi");
+    }
+    
+}
