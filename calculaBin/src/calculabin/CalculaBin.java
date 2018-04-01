658ebd668fd536e76b11b972bc18d578cfb81c2b
limabia <bianca.lima.1504@gmail.com>
2018-04-01 16:59:47 -0300

    conversao para binario de numeros inteiros

--- calculaBin/src/calculabin/CalculaBin.java
+++ calculaBin/src/calculabin/CalculaBin.java
@@ -20,4 +20,23 @@ public class CalculaBin {
     // func multiplica int algoritmo de booth
     
     // retorna o resultado em binario
+    public static void main(String args[]) {
+        int[] num = {};
+        // int n = decimalParaBinario(18, num);
+        binario(3);
+        // System.out.println(n);
+    }
+    public static int binario(int numero) {
+        int d = numero;
+        StringBuilder num = new StringBuilder(); // StringBuilder: string mutavel
+        while (d > 0) {
+                int b = d % 2;
+                num.append(b);
+                d = d >> 1;   // deslocamento logico, preenche sempre com zero a esquerda
+        }
+        String binarioS = num.reverse().toString();
+        int binario = Integer.parseInt(binarioS);
+        System.out.println(binario);
+        return(binario);
+    }
 }
